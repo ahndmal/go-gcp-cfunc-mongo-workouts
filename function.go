61190425ce73e4e5b69dd7e8971f417277ee8dd5
malandr <quadr988@gmail.com>
2022-10-02 17:26:57 +0300

    init

--- function.go
+++ function.go
@@ -13,6 +13,30 @@ import (
 )
 
 func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
+	queryParams := req.URL.Query()
+
+	wType := queryParams.Get("wType")
+	wDate := queryParams.Get("wDate")
+	month := queryParams.Get("month")
+	comments := queryParams.Get("comments")
+
+	var paramName string
+	var param string
+
+	if len(wType) > 0 {
+		param = wType
+		paramName = "workout_type"
+	} else if len(wDate) > 0 {
+		param = wDate
+		paramName = "workout_date"
+	} else if len(month) > 0 {
+		param = month
+		paramName = "month"
+	} else if len(comments) > 0 {
+		param = comments
+		paramName = "comments"
+	}
+
 	uri := os.Getenv("DB_URL")
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
@@ -25,52 +49,28 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 	}()
 	coll := client.Database("workouts").Collection("workouts")
 
-	//params := req.URL.Query()
-	//if len(params.Get("type")) == 0 { // get by TYPE if requested
-	//	cursor, err := coll.Find(context.TODO(), bson.D{{"type", params.Get("type")}})
-	//	if err == mongo.ErrNoDocuments {
-	//		fmt.Printf("No document was found with the type %s\n", "type")
-	//		return
-	//	}
-	//	err2 := cursor.All(context.TODO(), &workouts)
-	//	if err2 != nil {
-	//		log.Panicln(err2)
-	//	}
-	//	jsonData, err := json.MarshalIndent(workouts, "", "    ")
-	//	if err != nil {
-	//		panic(err)
-	//	}
-	//	fmt.Fprint(writer, string(jsonData))
-	//}
-	filter := bson.D{}
-	opts := options.Find().SetSort(bson.D{{"record", -1}})
-	cursor, err := coll.Find(context.TODO(), filter, opts)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title %s\n", "title")
-		return
-	}
-	defer cursor.Close(context.TODO())
-
 	//var workouts []bson.D
 	//var workouts []Workout
 	workouts := make([]Workout, 0)
 
-	if err = cursor.All(context.TODO(), &workouts); err != nil {
+	// filter / query
+	filter := bson.D{{paramName, param}}
+	findOptions := options.Find().SetSort(bson.D{{"record", -1}}) // find using filter and sort
+	//cursor, err := coll.Find(context.TODO(), bson.D{{"workout_type", wType}})	// find by type
+	cursor, err := coll.Find(context.TODO(), filter, findOptions)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the %s %s \n", paramName, param)
+		return
+	}
+	err2 := cursor.All(context.TODO(), &workouts)
+	if err2 != nil {
+		log.Panicln(err2)
+	}
+	jsonData, err := json.MarshalIndent(workouts, "", "    ")
+	if err != nil {
 		panic(err)
 	}
-
-	for cursor.Next(context.TODO()) {
-		//var workout bson.D
-		var workout Workout
-		if err := cursor.Decode(&workout); err != nil {
-			log.Fatalln(err)
-		}
-		workouts = append(workouts, workout)
-		//fmt.Println(workout)
-	}
-
-	log.Printf("Type of workouts is %T", workouts) // reflect.TypeOf(workouts)
-	log.Println(workouts[0])
+	fmt.Fprint(writer, string(jsonData))
 
 	jsonData, err3 := json.Marshal(workouts)
 	if err3 != nil {
