bc20d4d06eaef4de3ea4225fd19bee5fa39839d0
malandr <quadr988@gmail.com>
2022-10-05 18:46:20 +0300

    find byu dates, layout

--- function.go
+++ function.go
@@ -58,7 +58,6 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 
 	//var workouts []bson.D
 	var workouts []Workout
-	//workouts := make([]Workout, 0)
 
 	if paramName == "month" && paramName2 == "year" {
 		//filter := bson.D{{paramName, param}, }
@@ -94,7 +93,7 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 		if err5 != nil {
 			log.Fatalln(err5)
 		}
-	} else {
+	} else if len(paramName) > 0 {
 		// filter / query
 		filter := bson.D{{paramName, param}}
 		findOptions := options.Find().SetSort(bson.D{{"record", -1}}) // find using filter and sort
@@ -104,6 +103,32 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 			fmt.Printf("No document was found with the %s %s \n", paramName, param)
 			return
 		}
+		err2 := cursor.All(context.TODO(), &workouts)
+		if err2 != nil {
+			log.Panicln(err2)
+		}
+		jsonData, err3 := json.MarshalIndent(workouts, "", "    ")
+		if err3 != nil {
+			panic(err)
+		}
+
+		req.Header.Add("Content-Type", "application/json")
+		//req.Header.Add("Accept", "application/json")
+		//req.Response.Header.Add("Content-Type", "application/json")
+
+		_, err5 := fmt.Fprint(writer, string(jsonData))
+		if err5 != nil {
+			log.Fatalln(err5)
+		}
+	} else {
+		filter := bson.D{}
+		findOptions := options.Find().SetSort(bson.D{{"record", -1}}) // find using filter and sort
+		cursor, err := coll.Find(context.TODO(), filter, findOptions)
+
+		if err != nil {
+			log.Println(err)
+		}
+
 		err2 := cursor.All(context.TODO(), &workouts)
 		if err2 != nil {
 			log.Panicln(err2)
