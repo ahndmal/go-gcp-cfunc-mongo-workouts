2e39e4feb9b0c085bc1ac146780dc49d2d193c92
andrii <quadr988@gmail.com>
2023-10-13 18:58:04 +0300

    headers

--- function.go
+++ function.go
@@ -73,15 +73,20 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 		workDate, err := time.Parse(time.RFC822, fmt.Sprintf("01 %s %s 00:00 MST", month[0:3], year[2:4]))
 		log.Printf(">>> parsed date is : %s", fmt.Sprintf("01 %s %s 00:00 MST", month[0:3], year[2:4]))
 
+		// 2023-08-25T12:25:22.102+00:00
+
 		cursor, err4 := coll.Find(context.TODO(), bson.M{"created_at": bson.M{
 			"$gte": primitive.NewDateTimeFromTime(workDate),
 		}})
+
 		//findOptions := options.Find().SetSort(bson.D{{"record", -1}}) // find using filter and sort
 		//cursor, err4 := coll.Find(context.TODO(), filter, findOptions)
+
 		if errors.Is(err4, mongo.ErrNoDocuments) {
 			fmt.Printf("No document was found with the %s %s \n", paramName, param)
 			return
 		}
+
 		err2 := cursor.All(context.TODO(), &workouts)
 		if err2 != nil {
 			log.Panicln(err2)
@@ -92,6 +97,7 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 		}
 
 		req.Header.Add("Content-Type", "application/json")
+
 		_, err5 := fmt.Fprint(writer, string(jsonData))
 		if err5 != nil {
 			log.Fatalln(err5)
@@ -99,7 +105,8 @@ func GetWorkouts(writer http.ResponseWriter, req *http.Request) {
 	} else if len(paramName) > 0 {
 		// filter / query
 		filter := bson.D{{paramName, param}}
-		findOptions := options.Find().SetSort(bson.D{{"record", -1}}) // find using filter and sort
+		findOptions := options.Find().
+			SetSort(bson.D{{"record", -1}}) // find using filter and sort
 
 		//cursor, err := coll.Find(context.TODO(), bson.D{{"workout_type", wType}})	// find by type
 		cursor, err4 := coll.Find(context.TODO(), filter, findOptions)
